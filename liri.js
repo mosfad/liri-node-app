dcd3dd22471170204bb543e0bb6a2ecf03b8962b
Dupe Fadina <maafadina@hotmail.com>
2019-06-28 02:26:29 -0700

    Added axios get requests for OMDb API and Bandsintown API; Successfully displayed formatted results from OMDb API.

--- liri.js
+++ liri.js
@@ -9,12 +9,16 @@ var inquirer = require("inquirer");
 
 
 var spotify = new Spotify(keys.spotify);
+
+
 //key id should be spotify.id(see keys.js)
 //key secret should be spotify.secret(also see keys.js)
 //Get key from Bandsintown to create queryURL
 //----------------------var bandsInTown = new BandsInTown(keys.bandsInTown);
 //Get key from OMDb to create queryURL
 //----------------------var omdb = new Omdb(keys.omdb);
+// console.log(keys.bandsInTown.secret);
+// axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsInTown.secret)
 
 function queryTerm(userInput) {
     //Look for index of first space
@@ -31,7 +35,7 @@ inquirer.prompt([
     
 ]).then(function(user) {
     if (user.searchRequest.includes("spotify-this-song")) {
-        //If user requested a song, then use spotify to get info.
+        //If user requested a song, then use spotify API to get song info.
         spotify.search({ type: 'track', query: queryTerm(user.searchRequest) })
         .then(function(response) {
             var data = response.tracks.items[0];
@@ -47,7 +51,44 @@ inquirer.prompt([
             console.log(err);
         });
     }
+    else if (user.searchRequest.includes("movie-this")) {
+        //if user requested a movie, then use OMDB API to get movie info
+        axios.get("https://www.omdbapi.com/?t=" + queryTerm(user.searchRequest) + "&apikey=" + keys.omdb.secret)
+        .then(function (response) {
+            //handle success
+            console.log(response.data);
+            console.log("Movie Title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[0].Source); //NEED TO FIGURE OUT HOW TO EXTRACT ROTTEN TOMATOES RATINGS!!!
+            console.log("Country Produced: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        }) 
+        .catch(function (error) {
+            //handle error
+            console.log("error");
+        })                                                                                                                                     
+    }
+    else if (user.searchRequest.includes("concert-this")) {
+        //if user requested concert-this, then use Bandsintown API to get concert info
+        axios.get("https://rest.bandsintown.com/artists/" + queryTerm(user.searchRequest) + "/events?app_id=" + keys.bandsInTown.secret)
+        .then(function (response) {
+            // handle success
+            console.log(response);
+            console.log("Name of Venue");
+            console.log("Location of Venue: ");
+            console.log("Date of Event(MM/DD/YYYY): ");   //use moment.js for this
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+       
+    }
 
 
 })
 
+//Desktop/UCRX/projects/liri-node-app
